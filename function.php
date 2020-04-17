32ee1e6d69146fee337a4cd8ffc5418ab90b5e94
MatyasBaBca <47145006+MatyasBaBca@users.noreply.github.com>
2020-04-17 14:31:32 +0200

    Add files via upload

--- function.php
+++ function.php
@@ -1,13 +1,15 @@
-<?php 
-function vypoctiObvod($a, $b, $c){
-	return 4*($a+$b+$c);
+<?php
+function vypocetObvod($a, $b, $c)
+{
+    return 4*($a+$b+$c);
 }
 
-function vypoctiObsah($a, $b, $c){
-	return ($a*$b + $b*$c + $a*$c)*2;
+function vypocetObsah($a, $b, $c)
+{
+    return ($a*$b + $b*$c + $a*$c)*2;
 }
 
-function vypoctiObjem($a, $b, $c){
-	return $a*$b*$c;
+function vypocetObjem($a, $b, $c)
+{
+    return $a*$b*$c;
 }
-?>
